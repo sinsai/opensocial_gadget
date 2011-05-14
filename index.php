bc97e8b6cb9096358bf0f927ebdb2c0d0489f696
Nachi Ueno <nati.ueno@gmail.com>
2011-05-14 15:52:51 +0900

    analitcs

--- index.php
+++ index.php
@@ -108,6 +108,14 @@ google.friendconnect.container.renderOpenSocialGadget(
    height:$(window).height(),
    site: '05994095657403771775' });
 </script>
+<script type="text/javascript"> 
+		var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+		document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+		</script> 
+		<script type="text/javascript"> 
+		var pageTracker = _gat._getTracker("UA-22075443-1");
+		pageTracker._trackPageview();
+</script>
 </body>
 </html>
 <?php
